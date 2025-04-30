70ea1c0ed0729f75736596ad861a8a3a9d9340ae
Sam_Rgab <samragab1981@gmail.com>
2025-04-30 15:41:03 +0200

    Update index.py

--- index_model/index.py
+++ index_model/index.py
@@ -1,1 +1,16 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
 
+class IndexModel:
+    def __init__(self) -> None:
+        """Initialize the index model."""
+        pass
+
+    def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> None:
+        """Calculate index levels."""
+        pass
+
+    def export_values(self, file_name: str) -> None:
+        """Export calculated index levels to CSV."""
+        pass
